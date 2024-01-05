4551505b541cc005cc7ec10fefb5a909b279bcdf
lillyn123 <lillyn123@users.noreply.github.com>
2024-01-04 18:43:54 -0800

    Fixed daily tips
    Added secondTip variable with more tips then configured card CSS.

--- src/index.js
+++ src/index.js
@@ -43,14 +43,22 @@ form.addEventListener("submit", function (event) {
 });
 function displayTemperature(response) {
   let dailyTemperature = document.querySelector("#dailyTemperature");
-  let temperature = response.data.temperature;
+  let temperature = response.data.temperature.current;
   dailyTemperature.textContent = `${Math.round(
     response.data.temperature.current
   )}℃`;
+  // Temperature tip
+  let firstTip = document.querySelector('#firstTip');
   if (temperature <= 14) {
-    let firstTip = document.querySelector('#firstTip');
-    firstTip.textContent = 'Stay warm!';
+    firstTip.textContent = 'Wear something warm!';
   }
+  if (temperature >= 25) {
+    firstTip.textContent = "Stay dehydrated and drink water!";
+  }
+  if (temperature > 14 && temperature < 25) {
+    firstTip.textContent = "It feels nice outside."
+  } 
+  let secondTip = document.querySelector('#secondTip');
   let dailyCondition = document.querySelector("#day-conditon");
   let condition = response.data.condition.description;
   dailyCondition.textContent = `${day} | ${condition}`;
@@ -58,22 +66,30 @@ function displayTemperature(response) {
   if (condition === 'few clouds' || condition === 'scattered clouds' || condition === 'broken clouds' || condition === 'overcast clouds') {
     dailyIcon.classList.remove('fa-regular', 'fa-sun', 'fa-moon');
     dailyIcon.classList.add('fa-solid', 'fa-cloud');
+    secondTip.textContent = "Look at the sky!"
   }
   if (condition === 'rain' || condition === 'shower rain') {
     dailyIcon.classList.remove('fa-regular', 'fa-sun', 'fa-moon');
     dailyIcon.classList.add('fa-solid', 'fa-cloud-rain');
+    secondTip.textContent = "Stay inside."
   }
   if (condition === 'thunderstorm') {
     dailyIcon.classList.remove('fa-regular', 'fa-sun', 'fa-moon');
     dailyIcon.classList.add('fa-solid', 'fa-cloud-bolt');
+    secondTip.textContent = "It might be loud."
   }
   if (condition === 'snow') {
     dailyIcon.classList.remove('fa-regular', 'fa-sun', 'fa-moon');
     dailyIcon.classList.add('fa-solid', 'fa-snowflake');
+    secondTip.textContent = "Snowball fight!"
   }
   if (condition === 'mist') {
     dailyIcon.classList.remove('fa-regular', 'fa-sun', 'fa-moon');
     dailyIcon.classList.add('fa-solid', 'fa-wind');
+    secondTip.textContent = "Drive safe!"
+  }
+  if (condition === 'clear sky') {
+    secondTip.textContent = "Go for a walk."
   }
   let dailyHumidity = document.querySelector("#humidityValue");
   let humidity = response.data.temperature.humidity;
